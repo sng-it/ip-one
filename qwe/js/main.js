72cccf9c669725e55e9ae7b39db509a7e3e02b01
NomAZK <amanzhol1007@gmail.com>
2019-03-15 16:32:31 +0600

    Signed-off-by: NomAZK <amanzhol1007@gmail.com>

--- qwe/js/main.js
+++ qwe/js/main.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
+	$('a[href*="#"]')
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function(event) {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, function() {
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) {
+						return false;
+					} else {
+						$target.attr('tabindex','-1');
+						$target.focus();
+					};
+				});
+			}
+		}
+	});
 	$('.question h4').click(function(){
 		if($(this).children('.q_btn').hasClass('change')){
 			$(this).children('.q_btn').removeClass('change');
