9256615090a3bd77ebebb4281949927e45d76f1a
NomAZK <amanzhol1007@gmail.com>
2019-03-15 17:04:01 +0600

    Signed-off-by: NomAZK <amanzhol1007@gmail.com>

--- qwe/js/main.js
+++ qwe/js/main.js
@@ -1,27 +1,7 @@
 $(document).ready(function(){
-	$('a[href*="#"]')
-		.not('[href="#"]')
-		.not('[href="#0"]')
-		.click(function(event) {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				event.preventDefault();
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 1000, function() {
-					var $target = $(target);
-					$target.focus();
-					if ($target.is(":focus")) {
-						return false;
-					} else {
-						$target.attr('tabindex','-1');
-						$target.focus();
-					};
-				});
-			}
-		}
+	$(".toUp_btn").click(function() {
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
 	});
 	$('.question h4').click(function(){
 		if($(this).children('.q_btn').hasClass('change')){
